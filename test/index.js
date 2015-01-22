de3729b88c634b522d6664c5d15136b66252347c
Manuel Araoz <manuelaraoz@gmail.com>
2015-01-22 18:33:38 -0300

    initial structure

---
+++ test/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+var timelock = require('./');
+
+describe('timelock', function() {
+
+  it('exists', function() {
+    should.exist(timelock);
+  });
+
+});
